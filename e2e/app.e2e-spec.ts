9f3534dd3b76263fa3c05a84ae86529c5f4087a3
Octavio Franceschetto <octaviofranceschetto@gmail.com>
2017-08-23 16:59:40 -0300

    Add files via upload
    Arquivos do app calculadora.

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,9 @@
+import { CalculadoraPage } from './app.po';
+
+describe('calculadora App', () => {
+  let page: CalculadoraPage;
+
+  beforeEach(() => {
+    page = new CalculadoraPage();
+  });
+});
